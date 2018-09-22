feb32af5cad80f5389d656be54b6a9843fd42ed1
Jessica Chernyak <jessica@Jessicas-MacBook-Pro.local>
2018-09-22 15:47:58 -0500

    display form after submission, with encrypted string in textarea

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ form = """<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
       <form method="POST">
         <label for="rotate">Rotate by:</label>
         <input id ="rotate" type="text" name="rot" value="0" />
-        <textarea name="text"></textarea>
+        <textarea name="text">{0}</textarea>
         <input type="submit" name="Submit Query" />
       </form>
     </body>
@@ -36,13 +36,13 @@ form = """<!DOCTYPE html>
 
 @app.route("/")
 def index():
-    return form
+    return form.format("")
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = request.form['rot']
     text = request.form['text']
     new_string = rotate_string(text, int(rot))
-    return '<h1>' + new_string + '</h1>'
+    return form.format(new_string)
 
 app.run()
