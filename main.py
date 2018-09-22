9119cccf2368dbc56f3d5e1aa6d5ea9e0ac58b70
Jessica Chernyak <jessica@Jessicas-MacBook-Pro.local>
2018-09-22 15:19:45 -0500

    created a form and displayed results in a heading

---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+      <form method="POST">
+        <label for="rotate">Rotate by:</label>
+        <input id ="rotate" type="text" name="rot" value="0" />
+        <textarea name="text"></textarea>
+        <input type="submit" name="Submit Query" />
+      </form>
+    </body>
+</html>"""
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = request.form['rot']
+    text = request.form['text']
+    new_string = rotate_string(text, int(rot))
+    return '<h1>' + new_string + '</h1>'
+
+app.run()
